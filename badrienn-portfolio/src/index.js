0cfa20448dcb749690fcd17260edf71ef2427413
badrienn <adrienn.balazs.93@gmail.com>
2023-06-22 12:35:52 +0200

    create react router

--- badrienn-portfolio/src/index.js
+++ badrienn-portfolio/src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import './global.css';
+import Homepage from './pages/Homepage'
+import Layout from './pages/Layout'
+import CVpage from './pages/CVpage'
+import SchoolProjectsPage from './pages/SchoolProjectsPage'
+import PersonalProjectsPage from './pages/PersonalProjectsPage'
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />
+    },
+    {
+      path: "/pages",
+      element: <Layout />,
+      children: [
+        {
+          path: "/pages/cv",
+          element: <CVpage />
+        },
+        {
+          path: "/pages/school-projects",
+          element: <SchoolProjectsPage />
+        },
+        {
+          path: "/pages/personal-projects",
+          element: <PersonalProjectsPage />
+        }
+      ]
+    ,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
 );
-
