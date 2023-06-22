a2fa7faa50805d7dd0facc84637193f52d68a941
badrienn <adrienn.balazs.93@gmail.com>
2023-06-22 19:16:31 +0200

    add hamburger menu to mobile view

--- badrienn-portfolio/src/components/Navbar.js
+++ badrienn-portfolio/src/components/Navbar.js
@@ -1,10 +1,15 @@
 import { NavLink } from "react-router-dom";
+import { HiMenu } from 'react-icons/hi'
 import "../css/components-css/navbar.css"
+import { useState } from "react";
 
 export default function Navbar(){
+    const [isExpanded, setIsExpanded] = useState(false)
+
     return(
         <nav className="navbar-top">
-            <ul className="navbar-top-ul">
+            <button className="burger-btn" onClick={() => setIsExpanded(!isExpanded)}><HiMenu/></button>
+            <ul className={isExpanded ? "navbar-top-ul expanded-nav" : "navbar-top-ul"}>
                 <li><NavLink className={"navbar-top-navlink"} to="/">HOME</NavLink></li>
                 <li><NavLink className={"navbar-top-navlink"} to="/pages/cv">CV</NavLink></li>
                 <li><NavLink className={"navbar-top-navlink"} to="/pages/school-projects">SCHOOL PROJECTS</NavLink></li>
