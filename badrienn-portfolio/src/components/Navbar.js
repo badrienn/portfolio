0bdf02becf4875e2d215bf62d5e7227cd3d1a77f
badrienn <adrienn.balazs.93@gmail.com>
2023-06-27 00:08:48 +0200

    fix navbar on top of the screen

--- badrienn-portfolio/src/components/Navbar.js
+++ badrienn-portfolio/src/components/Navbar.js
@@ -7,13 +7,13 @@ export default function Navbar(){
     const [isExpanded, setIsExpanded] = useState(false)
 
     return(
-        <nav className="navbar-top">
+        <nav className="navbar-top light-bg">
             <button className="burger-btn" onClick={() => setIsExpanded(!isExpanded)}><HiMenu/></button>
             <ul className={isExpanded ? "navbar-top-ul expanded-nav" : "navbar-top-ul"}>
                 <li><NavLink className={"navbar-top-navlink"} to="/"><span>HOME</span></NavLink></li>
-                <li><NavLink className={"navbar-top-navlink"} to="/pages/cv"><span>RESUMÉ</span></NavLink></li>
-                <li><NavLink className={"navbar-top-navlink"} to="/pages/school-projects"><span>SCHOOL PROJECTS</span></NavLink></li>
-                <li><NavLink className={"navbar-top-navlink"} to="/pages/personal-projects"><span>PERSONAL PROJECTS</span></NavLink></li>
+                <li><NavLink className={"navbar-top-navlink"} to="/cv"><span>RESUMÉ</span></NavLink></li>
+                <li><NavLink className={"navbar-top-navlink"} to="/school-projects"><span>SCHOOL PROJECTS</span></NavLink></li>
+                <li><NavLink className={"navbar-top-navlink"} to="/personal-projects"><span>PERSONAL PROJECTS</span></NavLink></li>
             </ul>
         </nav>
     )
